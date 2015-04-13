8e0be75eb3cb58548bfbb7eb97402f679ec2cada
jarrahwu <jarrahwu@gmail.com>
2015-04-13 20:40:45 +0800

    bd map

--- all/MyApplication/app/src/main/java/com/jaf/biubiu/ActivityMain.java
+++ all/MyApplication/app/src/main/java/com/jaf/biubiu/ActivityMain.java
@@ -1,14 +1,18 @@
 package com.jaf.biubiu;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.jaf.jcore.BaseActionBarActivity;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
 
 
-public class ActivityMain extends BaseActionBarActivity {
+public class ActivityMain extends ActionBarActivity {
 
+
+    private MapView mMapView;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -28,12 +32,30 @@ public class ActivityMain extends BaseActionBarActivity {
     }
 
     @Override
-    protected int onLoadViewResource() {
-        return R.layout.activity_main;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        mMapView = (MapView) findViewById(R.id.bmapView);
     }
 
-    @Override
-    protected void onViewDidLoad(Bundle savedInstanceState) {
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
     }
 }
