144f5920f0d3c562e65aed490241355948e41d6b
jarrahwu <jarrahwu@gmail.com>
2015-04-13 20:59:17 +0800

    add loc

--- all/MyApplication/app/src/main/java/com/jaf/biubiu/ActivityMain.java
+++ all/MyApplication/app/src/main/java/com/jaf/biubiu/ActivityMain.java
@@ -2,9 +2,13 @@ package com.jaf.biubiu;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.MapView;
 
@@ -13,6 +17,9 @@ public class ActivityMain extends ActionBarActivity {
 
 
     private MapView mMapView;
+
+    public LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,6 +44,10 @@ public class ActivityMain extends ActionBarActivity {
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
         mMapView = (MapView) findViewById(R.id.bmapView);
+
+        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);    //注册监听函数
+        mLocationClient.start();
     }
 
 
@@ -57,5 +68,36 @@ public class ActivityMain extends ActionBarActivity {
         super.onPause();
         //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
         mMapView.onPause();
+    }
+
+
+    public class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            if (location == null)
+                return ;
+            StringBuffer sb = new StringBuffer(256);
+            sb.append("time : ");
+            sb.append(location.getTime());
+            sb.append("\nerror code : ");
+            sb.append(location.getLocType());
+            sb.append("\nlatitude : ");
+            sb.append(location.getLatitude());
+            sb.append("\nlontitude : ");
+            sb.append(location.getLongitude());
+            sb.append("\nradius : ");
+            sb.append(location.getRadius());
+            if (location.getLocType() == BDLocation.TypeGpsLocation){
+                sb.append("\nspeed : ");
+                sb.append(location.getSpeed());
+                sb.append("\nsatellite : ");
+                sb.append(location.getSatelliteNumber());
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation){
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+           }
+
+            Log.e("", sb.toString());
+        }
     }
 }
