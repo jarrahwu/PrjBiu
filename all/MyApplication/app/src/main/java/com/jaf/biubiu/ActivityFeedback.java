268d90f02d11025cfd967a895616f266f70ddd9f
jarrahwu <jarrahwu@gmail.com>
2015-04-23 11:53:49 +0800

    add danmu lib

--- all/MyApplication/app/src/main/java/com/jaf/biubiu/ActivityFeedback.java
+++ all/MyApplication/app/src/main/java/com/jaf/biubiu/ActivityFeedback.java
@@ -2,6 +2,7 @@ package com.jaf.biubiu;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -18,6 +19,23 @@ import com.jaf.jcore.HttpCallBack;
 
 import org.json.JSONObject;
 
+import java.io.InputStream;
+
+import master.flame.danmaku.controller.DrawHandler;
+import master.flame.danmaku.danmaku.loader.ILoader;
+import master.flame.danmaku.danmaku.loader.IllegalDataException;
+import master.flame.danmaku.danmaku.loader.android.DanmakuLoaderFactory;
+import master.flame.danmaku.danmaku.model.BaseDanmaku;
+import master.flame.danmaku.danmaku.model.Danmaku;
+import master.flame.danmaku.danmaku.model.DanmakuTimer;
+import master.flame.danmaku.danmaku.model.android.DanmakuGlobalConfig;
+import master.flame.danmaku.danmaku.model.android.Danmakus;
+import master.flame.danmaku.danmaku.parser.BaseDanmakuParser;
+import master.flame.danmaku.danmaku.parser.DanmakuFactory;
+import master.flame.danmaku.danmaku.parser.IDataSource;
+import master.flame.danmaku.danmaku.parser.android.BiliDanmukuParser;
+import master.flame.danmaku.ui.widget.DanmakuView;
+
 
 public class ActivityFeedback extends BaseActionBarActivity {
 
@@ -27,6 +45,11 @@ public class ActivityFeedback extends BaseActionBarActivity {
     @BindView(id = R.id.send, onClick = "onSendClick")
     private View mSend;
 
+    @BindView(id = R.id.danmu)
+    private DanmakuView mDanmakuView;
+
+    private BaseDanmakuParser mParser;
+
     @Override
     protected int onLoadViewResource() {
         return R.layout.activity_feedback;
@@ -35,7 +58,75 @@ public class ActivityFeedback extends BaseActionBarActivity {
     @Override
     protected void onViewDidLoad(Bundle savedInstanceState) {
         mContent.setHint(R.string.feedback_tips);
+        mParser = createParser(this.getResources().openRawResource(R.raw.empty));
+        DanmakuGlobalConfig.DEFAULT.setDanmakuStyle(DanmakuGlobalConfig.DANMAKU_STYLE_STROKEN, 3).setDuplicateMergingEnabled(false);
+        mDanmakuView.setCallback(new DrawHandler.Callback() {
+
+            @Override
+            public void updateTimer(DanmakuTimer timer) {
+
+            }
+
+            @Override
+            public void prepared() {
+                mDanmakuView.start();
+            }
+        });
+        mDanmakuView.enableDanmakuDrawingCache(true);
+        mDanmakuView.prepare(mParser);
+        mDanmakuView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addDanmaku(false);
+            }
+        });
     }
+
+    private BaseDanmakuParser createParser(InputStream stream) {
+
+        if(stream==null){
+            return new BaseDanmakuParser() {
+
+                @Override
+                protected Danmakus parse() {
+                    return new Danmakus();
+                }
+            };
+        }
+
+
+        ILoader loader = DanmakuLoaderFactory.create(DanmakuLoaderFactory.TAG_BILI);
+
+        try {
+            loader.load(stream);
+        } catch (IllegalDataException e) {
+            e.printStackTrace();
+        }
+        BaseDanmakuParser parser = new BiliDanmukuParser();
+        IDataSource<?> dataSource = loader.getDataSource();
+        parser.load(dataSource);
+        return parser;
+
+    }
+
+    private void addDanmaku(boolean islive) {
+        BaseDanmaku danmaku = DanmakuFactory.createDanmaku(BaseDanmaku.TYPE_SCROLL_RL, 200f, 100f, 1f);
+//        BaseDanmaku danmaku = DanmakuFactory.createDanmaku(BaseDanmaku.TYPE_SCROLL_RL);
+        //for(int i=0;i<100;i++){
+        //}
+        danmaku.text = "这是一条弹幕, 继续点击屏幕吧" + System.nanoTime();
+        danmaku.padding = 5;
+        danmaku.priority = 1;
+        danmaku.isLive = islive;
+        danmaku.time = mDanmakuView.getCurrentTime() + 1200;
+        danmaku.textSize = 25f;
+        danmaku.textColor = Color.RED;
+        danmaku.textShadowColor = Color.parseColor("#aabbcc");
+        //danmaku.underlineColor = Color.GREEN;
+        danmaku.borderColor = Color.GREEN;
+        mDanmakuView.addDanmaku(danmaku);
+    }
+
 
     public static void start(Activity activity) {
         activity.startActivity(new Intent(activity, ActivityFeedback.class));
